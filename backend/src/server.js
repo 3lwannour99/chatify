b4cd775644fdbe0ba94337f3f6dc1b93f5b6ab7c
nour <nour3lwan99@gmail.com>
2025-09-16 21:14:16 +0300

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from 'path'
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js"
+
 const app = express();
+const __dirname = path.resolve();
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -10,6 +15,15 @@ const PORT = process.env.PORT || 3000;
 //prefex 
 app.use("/api/auth", authRoutes); 
 app.use("/api/message",messageRoutes);
+
+// make ready for deployment
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname , "../frontend/dist")))
+// if u visit any route exept the our routes , the frontend will appear
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname , "../frontend","dist","index.html"))
+    })
+}
 
 
 
