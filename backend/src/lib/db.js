232da32fa7501cc731ad51b0876457203f1b1fcc
nour <nour3lwan99@gmail.com>
2025-09-17 13:18:05 +0300

    refactoring code rabbit

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-export const connectDB = async ()=>{
-    try {
-     const conn =  await mongoose.connect(process.env.MONGO_URI)
-        console.log("MONGODB connected successfully",conn.connection.host)
-    } catch (error) {
-        console.log("Error connecting to MONGODB",error)
-        process.exit(1)// 1 means fail , 0 means success
-    }
-}
+export const connectDB = async () => {
+  try {
+    const { MONGO_URI } = process.env;
+    if (!MONGO_URI) throw new Error("NONGO_URI is not set");
+
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MONGODB connected successfully", conn.connection.host);
+  } catch (error) {
+    console.log("Error connecting to MONGODB", error);
+    process.exit(1); // 1 means fail , 0 means success
+  }
+};
