01049887319bda08567d278341b80f142b0f6ae8
nour <nour3lwan99@gmail.com>
2025-09-29 00:06:59 +0300

    login & logout added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -58,8 +58,6 @@ export const signup = async (req, res) => {
       } catch (error) {
         console.error("faild to send welcome email: ", error);
       }
-
-      
     } else {
       res.status(400).json({ message: "Invalid user data" });
     }
@@ -68,4 +66,36 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: "Enternal server error" });
   }
 };
- 
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+    // never tell the client which one is not correct
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+
+    console.error("error in login controller", error)
+    res.status(500).json({message:"Internal Server Error"})
+  }
+};
+
+export const logout =  (_, res) => {
+
+  res.cookie("jwt","",{maxAge:0}) // jwt same in the utils
+  res.status(200).json({message:"Logged out succesffully"})
+};
